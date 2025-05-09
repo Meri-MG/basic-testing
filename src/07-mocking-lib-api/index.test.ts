59004e66c9ac2d939c33a95d8f695055acf4478f
Meri-MG <mgogi13@freeuni.edu.ge>
2025-05-09 08:54:19 +0400

    feat: pass mocking lib api tests

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,69 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
 
+jest.mock('axios');
 describe('throttledGetDataFromApi', () => {
+  const url = '/albums'
+
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.clearAllTimers();
+  });
+  
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mockedGet = jest.fn().mockResolvedValue({ data: [
+      {
+        userId: 1,
+        id: 1,
+        title: 'My First Album'
+      }
+    ]});
+    const mockedCreate = jest.fn().mockReturnValue({ get: mockedGet });
+    (axios.create as jest.Mock) = mockedCreate;
+
+    await throttledGetDataFromApi(url);
+    jest.advanceTimersByTime(5000); 
+
+    expect(mockedCreate).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const mockedGet = jest.fn().mockResolvedValue({ data: [
+      {
+        userId: 1,
+        id: 1,
+        title: 'My First Album'
+      }
+    ]});
+    (axios.create as jest.Mock).mockReturnValue({ get: mockedGet });
+
+    await throttledGetDataFromApi(url);
+    jest.advanceTimersByTime(5000); 
+
+    expect(mockedGet).toHaveBeenCalledWith(url);
   });
 
-  test('should return response data', async () => {
-    // Write your test here
+  test('should return response data', async () => {  
+    const mockedGet = jest.fn().mockResolvedValue({ data: [
+      {
+        userId: 1,
+        id: 1,
+        title: 'My First Album'
+      }
+    ]});
+    (axios.create as jest.Mock).mockReturnValue({ get: mockedGet });
+    const throttled = throttledGetDataFromApi(url);
+    jest.advanceTimersByTime(5000);
+    const data = await throttled;
+
+    expect(data[0].title).toEqual('My First Album');
+    expect(mockedGet).toHaveBeenCalledWith(url);
   });
 });
